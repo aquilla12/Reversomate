336d266097292864d1ff7b5c332ab88b305fc972
aquilla12 <24236468+aquilla12@users.noreply.github.com>
2018-01-30 08:44:19 +1300

    Upload project

---
+++ Reversomate/Main window.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Reversomate
+{
+    partial class Reversomate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Reversomate));
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.seperatorBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.run = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.importButton = new System.Windows.Forms.Button();
+            this.exportButton = new System.Windows.Forms.Button();
+            this.description = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.doneButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputBox
+            // 
+            this.inputBox.AcceptsReturn = true;
+            this.inputBox.AcceptsTab = true;
+            this.inputBox.Location = new System.Drawing.Point(15, 34);
+            this.inputBox.Multiline = true;
+            this.inputBox.Name = "inputBox";
+            this.inputBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.inputBox.Size = new System.Drawing.Size(233, 317);
+            this.inputBox.TabIndex = 1;
+            this.toolTip1.SetToolTip(this.inputBox, "Please enter your input text");
+            // 
+            // outputBox
+            // 
+            this.outputBox.AcceptsReturn = true;
+            this.outputBox.AcceptsTab = true;
+            this.outputBox.Location = new System.Drawing.Point(289, 34);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ReadOnly = true;
+            this.outputBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputBox.Size = new System.Drawing.Size(233, 317);
+            this.outputBox.TabIndex = 0;
+            this.outputBox.TabStop = false;
+            this.toolTip1.SetToolTip(this.outputBox, "Output");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(286, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Output";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Input";
+            // 
+            // seperatorBox
+            // 
+            this.seperatorBox.Location = new System.Drawing.Point(289, 383);
+            this.seperatorBox.MaxLength = 1;
+            this.seperatorBox.Name = "seperatorBox";
+            this.seperatorBox.Size = new System.Drawing.Size(100, 20);
+            this.seperatorBox.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.seperatorBox, "Enter a character that will determine where the engine splits your input");
+            this.seperatorBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.seperatorBox_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(286, 364);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Seperate by";
+            // 
+            // run
+            // 
+            this.run.Location = new System.Drawing.Point(408, 381);
+            this.run.Name = "run";
+            this.run.Size = new System.Drawing.Size(75, 23);
+            this.run.TabIndex = 3;
+            this.run.Text = "Run";
+            this.toolTip1.SetToolTip(this.run, "Run the engine to generate an output");
+            this.run.UseVisualStyleBackColor = true;
+            this.run.Click += new System.EventHandler(this.run_Click);
+            // 
+            // importButton
+            // 
+            this.importButton.Location = new System.Drawing.Point(168, 8);
+            this.importButton.Name = "importButton";
+            this.importButton.Size = new System.Drawing.Size(80, 23);
+            this.importButton.TabIndex = 4;
+            this.importButton.Text = "Import";
+            this.toolTip1.SetToolTip(this.importButton, "Import from .txt document");
+            this.importButton.UseVisualStyleBackColor = true;
+            this.importButton.Click += new System.EventHandler(this.importButton_Click);
+            // 
+            // exportButton
+            // 
+            this.exportButton.Enabled = false;
+            this.exportButton.Location = new System.Drawing.Point(442, 8);
+            this.exportButton.Name = "exportButton";
+            this.exportButton.Size = new System.Drawing.Size(80, 23);
+            this.exportButton.TabIndex = 4;
+            this.exportButton.Text = "Export";
+            this.toolTip1.SetToolTip(this.exportButton, "Export to a .txt document");
+            this.exportButton.UseVisualStyleBackColor = true;
+            this.exportButton.Click += new System.EventHandler(this.exportButton_Click);
+            // 
+            // description
+            // 
+            this.description.Location = new System.Drawing.Point(15, 364);
+            this.description.Name = "description";
+            this.description.Size = new System.Drawing.Size(233, 80);
+            this.description.TabIndex = 0;
+            this.description.Text = "This engine will take your input text, split it at every occurence of your specif" +
+    "ied character, reverse the order of these sub-strings and compile it back togeth" +
+    "er in the output box.";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Text files|*.txt";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "txt";
+            this.saveFileDialog1.Filter = "Text files|*.txt";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(289, 423);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(155, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Display horizontal scrollbars";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // doneButton
+            // 
+            this.doneButton.Location = new System.Drawing.Point(447, 8);
+            this.doneButton.Name = "doneButton";
+            this.doneButton.Size = new System.Drawing.Size(75, 23);
+            this.doneButton.TabIndex = 6;
+            this.doneButton.Text = "Done";
+            this.doneButton.UseVisualStyleBackColor = true;
+            this.doneButton.Visible = false;
+            this.doneButton.Click += new System.EventHandler(this.doneButton_Click);
+            // 
+            // Reversomate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 455);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.importButton);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.description);
+            this.Controls.Add(this.run);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.seperatorBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.exportButton);
+            this.Controls.Add(this.doneButton);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(550, 494);
+            this.MinimumSize = new System.Drawing.Size(550, 494);
+            this.Name = "Reversomate";
+            this.Text = "Reversomate";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox seperatorBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button run;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label description;
+        private System.Windows.Forms.Button importButton;
+        private System.Windows.Forms.Button exportButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button doneButton;
+    }
+}
+
---
+++ Reversomate/Main window.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reversomate
+{
+    public partial class Reversomate : Form
+    {
+        public Reversomate()
+        {
+            InitializeComponent();
+        }
+        public void ExportAvalible()
+        {
+            if (string.IsNullOrEmpty(outputBox.Text)) exportButton.Enabled = false;
+            else exportButton.Enabled = true;
+        }
+        public void runEngine()
+        {
+            if (!string.IsNullOrEmpty(seperatorBox.Text) && !string.IsNullOrEmpty(inputBox.Text))
+            {
+                char seperator = seperatorBox.Text.First();
+                string[] Input = inputBox.Text.Split(seperator);
+                Array.Reverse(Input);
+                string output = "";
+                foreach (string segment in Input)
+                {
+                    output += segment;
+                }
+                outputBox.Text = output;
+                ExportAvalible();
+            }
+            else if (string.IsNullOrEmpty(inputBox.Text))
+            {
+                MessageBox.Show("Cannot run with no input, please provide input text in the box provided.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning); ;
+                inputBox.Select();
+            }
+            else
+            {
+                MessageBox.Show("Please input a character to the 'Seperate by' box.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                seperatorBox.Select();
+            }
+        }
+        private void run_Click(object sender, EventArgs e)
+        {
+            runEngine();
+        }
+
+        private void importButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK) inputBox.Text = System.IO.File.ReadAllText(openFileDialog1.FileName);
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Sorry, something went wrong somwhere. Please try again or try something else.", "Import Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            inputBox.Select();
+            
+        }
+
+        private void exportButton_Click(object sender, EventArgs e)
+        {
+            inputBox.ReadOnly = true;
+            outputBox.ReadOnly = false;
+            seperatorBox.ReadOnly = true;
+            run.Enabled = false;
+            importButton.Enabled = false;
+            doneButton.Visible = true;
+            exportButton.Visible = false;
+            
+            if (MessageBox.Show("Please perform any edits you wish to make before export. Click the 'Done' button when ready.", "Edit output for export", MessageBoxButtons.OKCancel, MessageBoxIcon.Information) == DialogResult.Cancel)
+            {
+                inputBox.ReadOnly = false;
+                outputBox.ReadOnly = true;
+                seperatorBox.ReadOnly = false;
+                run.Enabled = true;
+                importButton.Enabled = true;
+                doneButton.Visible = false;
+                exportButton.Visible = true;
+                ExportAvalible();
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                inputBox.ScrollBars = ScrollBars.Both;
+                inputBox.WordWrap = false;
+                outputBox.ScrollBars = ScrollBars.Both;
+                outputBox.WordWrap = false;
+            }
+            else
+            {
+                inputBox.ScrollBars = ScrollBars.Vertical;
+                inputBox.WordWrap = true;
+                outputBox.ScrollBars = ScrollBars.Vertical;
+                outputBox.WordWrap = true;
+            }
+        }
+
+        private void doneButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK) System.IO.File.WriteAllText(saveFileDialog1.FileName, outputBox.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Sorry, something went wrong somwhere. Please try again or try something else.", "Export Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                inputBox.ReadOnly = false;
+                outputBox.ReadOnly = true;
+                seperatorBox.ReadOnly = false;
+                run.Enabled = true;
+                importButton.Enabled = true;
+                doneButton.Visible = false;
+                exportButton.Visible = true;
+                ExportAvalible();
+            }
+        }
+
+        private void seperatorBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Convert.ToInt32(e.KeyChar) == 13) runEngine();
+        }
+
+    }
+}
